478dab145cb0d02d35154cc781f0562f7cb198f8
ValeryKharkov <kharkov.valery@gmail.com>
2023-01-12 22:24:59 +0300

    Добавлены сигналы

--- Draft.py
+++ Draft.py
@@ -12,7 +12,7 @@ class Window(QtWidgets.QWidget):
 
         self.initSQLModel()
         self.initUi()  # Метод для инициализации интерфейса
-        #self.initSignals()
+        self.initSignals() # Метод для инициализации сигналов
 
     def initUi(self) -> None:
         """
@@ -21,7 +21,7 @@ class Window(QtWidgets.QWidget):
         :return: None
         """
 
-        self.setWindowTitle("БД")  # Установка заголовка окна
+        self.setWindowTitle("Телефонная книга")  # Установка заголовка окна
 
         # Создание виджетов Label
         labelFirstName = QtWidgets.QLabel('Имя')
@@ -77,6 +77,15 @@ class Window(QtWidgets.QWidget):
         self.ui.tableView.horizontalHeader().setSectionsMovable(True)
         self.ui.tableView.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
+    def initSignals(self) -> None:
+        """
+        Инициализация сигналов
+
+        :return: None
+        """
+        self.ui.pushButtonAdd.clicked.connect(self.onPushButtonAddClicked)
+        self.ui.pushButtonDel.clicked.connect(self.onPushButtonDelClicked)
+
     def initSQLModel(self) -> None:
         """
         Создание подключения (модели) для работы с БД
@@ -88,15 +97,45 @@ class Window(QtWidgets.QWidget):
         db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
         db.setDatabaseName('fieldlist.db')
 
+        # db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+        # db.setDatabaseName('PyQt_exam.db')
+
         # Создание модели
         self.model = QtSql.QSqlTableModel()
-        self.model.setTable('field')
-        self.model.select()
-        self.model.setHeaderData(1, QtCore.Qt.Horizontal, "Имя")
+        self.model.setTable('field')  # Выбор таблицы, с которой работает модель
+        self.model.select()  # Заполнение модели данными из таблицы 'field'
+        self.model.setHeaderData(1, QtCore.Qt.Horizontal, "Имя")  # Установка данных для наименование заголовков таблицы
         self.model.setHeaderData(2, QtCore.Qt.Horizontal, "Фамилия")
         self.model.setHeaderData(3, QtCore.Qt.Horizontal, "Телефон")
 
+    def onPushButtonAddClicked(self):
+        """
+        Обработка нажатия на кнопку "Добавить"
+        :return:
+        """
 
+        index = self.model.rowCount()
+        self.model.insertRows(index, 1)
+        self.model.setData(self.model.index(index, 1), self.ui.lineEditName.text())
+        self.model.setData(self.model.index(index, 2), self.ui.lineEditSurname.text())
+        self.model.setData(self.model.index(index, 3), self.ui.lineEditPhone.text())
+
+        self.model.submitAll()
+
+    def onPushButtonDelClicked(self) -> None:
+        """
+        Обработка нажатия на кнопку "Удалить"
+
+        :return: None
+        """
+
+        if self.ui.tableView.currentIndex().row() > 0:
+            self.model.removeRow(self.ui.tableView.currentIndex().row())
+            self.model.select()
+
+        else:
+            QtWidgets.QMessageBox.question(self, 'Уведомление', 'Пожалуйста, выберите строку для удаления',
+                                           QtWidgets.QMessageBox.Ok)
 
 
 if __name__ == "__main__":
